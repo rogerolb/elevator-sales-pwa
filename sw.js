d2bbf72d8f4ac1a46fd6f7bf3aee759f72c04317
rogerolb <roger.olb@gmail.com>
2026-09-21 13:39:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ev-sales-shell-v4.5';
+const CACHE='ev-sales-shell-v4.7';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./app_config.js','./cloud_adapter.js','./setup_v45.html','./connection_check_v45.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
